4b26647017ec9170289993bcfed829f602d3db34
13klove <13klove@naver.com>
2022-02-19 17:21:11 +0900

    Initial commit

---
+++ src/test/kotlin/com/m/one/meureka/MEurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.m.one.meureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MEurekaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
